00df42e444c6ae243c71e56f3ee546e0fb55e088
taka110 <bigtree.aska@gmail.com>
2020-11-30 01:10:32 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,2 +1,12 @@
+var sortNumber = function (number) {
+  number.sort(function (a, b) {
+    if (a == b) {
+      return 0;
+    }
+  });
+};
+
+
 var numver = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
 console.log(number);
